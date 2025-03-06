73de268cc9120c2e8c36f8b651b6374cc95608c9
happsie <njesper98@gmail.com>
2025-03-06 20:04:28 +0100

    remove update endpoint

--- internal/api.go
+++ internal/api.go
@@ -20,7 +20,6 @@ func StartWebserver(addr string, pool *pgxpool.Pool) {
 
 	mux.HandleFunc("POST /api/kiddo/track-type-v1", trackHandler.CreateTrackType)
 	mux.HandleFunc("GET /api/kiddo/track-type-v1", trackHandler.GetTrackTypes)
-	mux.HandleFunc("PUT /api/kiddo/track-type-v1/{trackID}", trackHandler.UpdateTrackType)
 	mux.HandleFunc("DELETE /api/kiddo/track-type-v1/{trackID}", trackHandler.DeleteTrackType)
 
 	mux.HandleFunc("GET /api/kiddo/track-events-v1", func(w http.ResponseWriter, r *http.Request) {
